87dc4f38080c6b492809973965e5020e08bfe05c
dgouron <damien.gouron@gmail.com>
2023-05-12 17:16:53 +0200

    Feat : homepage
    . Design of the page
    . Image optimization -> webp
    . Lighthouse optimization
    . Function factory for photographers
    . Photographers link

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -3,25 +3,82 @@ function photographerFactory(data) {
 
     const picture = `assets/portraits/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
+    const compilePhotographerArticle = (items) => {
+        const photographer = document.createElement( 'article' );
+        console.log(items);
+        items.forEach(item => {
+            photographer.appendChild(item);
+        });
+        return photographer;
+    };
+    const compileDetails = (items) => {
+        const details = document.createElement( 'div' );
+        details.id = "details";
+        details.classList.add("details");
+        items.forEach(item => {
+            details.appendChild(item);
+        });
+        return details;
+    };
+    const buildCardImg = () => {
+        const img = document.createElement( 'img' );
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", `Photo du photographe ${name}`);
+        return img;
+    };
+    const buildCardName = () => {
+        const h2 = document.createElement( 'h2' );
+        h2.textContent = name;
+        return h2;
+    };
+    const buildLocation = () => {
+        const city = document.createElement( 'h3' );
+        city.textContent = `${data.city}, ${data.country}`;
+        return city;
+    };
+
+    const buildTagline = () => {
+        const tagline = document.createElement( 'p' );
+        tagline.textContent = data.tagline;
+        return tagline;
+    };
+
+    const buildPrice = () => {
+        const price = document.createElement( 'p' );
+        price.textContent = `${data.price}€/jour`;
+        return price;
+    };
+
+    function buildCard(){
+        const card = document.createElement( 'div' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture);
         img.setAttribute("alt", `Photo du photographe ${name}`);
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        const city = document.createElement( 'h3' );
-        city.textContent = `${data.city}, ${data.country}`;
-        article.appendChild(city);
-        const tagline = document.createElement( 'p' );
-        tagline.textContent = data.tagline;
-        article.appendChild(tagline);
-        const price = document.createElement( 'p' );
-        price.textContent = `${data.price}€/jour`;
-        article.appendChild(price);
-        return (article);
+        card.appendChild(img);
+        card.appendChild(h2);
+        return card;
     }
-    return { name, picture, getUserCardDOM }
+
+    function buildLink(itemsToWrap) {
+        const linkToPhotographer = document.createElement( 'a' );
+        linkToPhotographer.setAttribute("href", `photographer.html?id=${data.id}`);
+        linkToPhotographer.setAttribute("aria-label", `lien vers ${name} profil`);
+        itemsToWrap.forEach(item => {
+            linkToPhotographer.appendChild(item);
+        });
+        return linkToPhotographer;
+    }
+
+    return { 
+        buildLink,
+        buildCardImg, 
+        buildCardName, 
+        compilePhotographerArticle,
+        compileDetails,
+        buildLocation,
+        buildTagline,
+        buildPrice,
+     }
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -2,8 +2,39 @@ async function displayData(photographers) {
     const photographersSection = document.querySelector(".photographer_section");
     photographers.forEach((photographer) => {
         const photographerModel = photographerFactory(photographer);
-        const userCardDOM = photographerModel.getUserCardDOM();
-        photographersSection.appendChild(userCardDOM);
+        /* Add card img */
+        const photographerCardImg = photographerModel.buildCardImg();
+        /* Add card name */
+        const photographerCardName = photographerModel.buildCardName();
+        /* Add country and city */
+        const photographerLocation = photographerModel.buildLocation();
+        /* Add tagline */
+        const photographerTagline = photographerModel.buildTagline();
+        /* Add price */
+        const photographerPrice = photographerModel.buildPrice();
+        
+        /* Wrap details in block */
+        const photographerDetails = photographerModel.compileDetails([
+            photographerLocation,
+            photographerTagline,
+            photographerPrice
+        ]);
+
+        /* Wrap card into link */
+        const photographerLink = photographerModel.buildLink([photographerCardImg, photographerCardName]);
+       
+        /* Add link to section */   
+        photographersSection.appendChild(photographerLink);
+
+        /* Create article */
+        const photographerArticle = 
+        photographerModel.compilePhotographerArticle([
+            photographerLink,
+            photographerDetails,
+        ]);
+
+        /* Add article to section */
+        photographersSection.appendChild(photographerArticle);
     });
 };
 
