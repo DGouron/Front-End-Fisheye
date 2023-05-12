68d2758c76dab890c7e031a592f88b9f8762a479
dgouron <damien.gouron@gmail.com>
2023-05-12 13:47:24 +0200

    Feat : design of main page
    Feat : design of main page

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -6,7 +6,8 @@ function photographerFactory(data) {
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", `Photo du photographe ${name}`);
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
         article.appendChild(img);
@@ -14,10 +15,10 @@ function photographerFactory(data) {
         const city = document.createElement( 'h3' );
         city.textContent = `${data.city}, ${data.country}`;
         article.appendChild(city);
-        const tagline = document.createElement( 'h4' );
+        const tagline = document.createElement( 'p' );
         tagline.textContent = data.tagline;
         article.appendChild(tagline);
-        const price = document.createElement( 'h5' );
+        const price = document.createElement( 'p' );
         price.textContent = `${data.price}€/jour`;
         article.appendChild(price);
         return (article);
